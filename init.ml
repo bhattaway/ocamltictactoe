25af4860264588970e23741e9bd30adca66b03b3
bhattaway <bhattaway98@gmail.com>
2018-03-04 16:51:36 -0600

    changed initial board to be all spaces instead of all X's

--- init.ml
+++ init.ml
@@ -10,7 +10,7 @@ handles initializing the board.
 let rec init_rows = fun n -> fun list ->
 	match n with
 	0 -> list
-	|	n -> init_rows (n-1) ('X'::list)
+	|	n -> init_rows (n-1) (' '::list)
 ;;
 
 let rec init_cols = fun n -> fun list -> fun acc ->
